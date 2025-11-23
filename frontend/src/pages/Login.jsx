4cecd67938aa44defd7a997b0f4d7b4bf90bab85
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2025-11-23 22:47:17 +0100

    updated some other stuffs

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
   const [form, setForm] = useState({ email: "", password: "" });
   const [msg, setMsg] = useState("");
+  const [isText, setIsText] = useState(false);
   const { login } = useAuth();
   const navigate = useNavigate();
+
+  const handleVisibility = () => {
+    setIsText((prev) => !prev);
+  };
 
   const handleChange = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value });
@@ -28,22 +34,55 @@ const Login = () => {
     }
   };
   return (
-    <div className="flex h-[50vh] items-center justify-center">
-      <div className="w-[50%] border-2 px-[40px] py-[50px] mt-[200px] border-gray-300">
+    <div className="flex h-[50vh] items-center justify-center p-2">
+      <div className="w-[100%] md:w-[50%] border-2 px-[10px] md:px-[40px] px-[15px] md:py-[50px] mt-[20px] md:mt-[200px] border-gray-300">
         <form onSubmit={handleSubmit}>
-          <div>
-            <h2 className="text-6xl text-center">Sign In</h2>
-            <h2>Access Your Account</h2>
+          <div className="mb-8">
+            <h2 className="text-3xl md:text-6xl text-center mb-2">Sign In</h2>
+            <h2 className="text-1xl md:text-2xl text-center">
+              Access Your Account
+            </h2>
           </div>
-          <input
-            name="email"
-            type="email"
-            placeholder="E-Mail"
-            onChange={handleChange}
-          />
-          <input name="password" type="password" onChange={handleChange} />
-          <button type="submit">Login</button>
-          <p>{msg}</p>
+
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col">
+              <label className="text-[24px]">E-Mail</label>
+              <input
+                name="email"
+                type="email"
+                placeholder="E-Mail"
+                onChange={handleChange}
+                className="text-black text-[22px] focus:outline-none border-2 border-gray-300 p-2"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="text-[24px]">Password</label>
+              <div className="border-2 border-gray-300 flex">
+                <input
+                  name="password"
+                  type={isText ? "text" : "password"}
+                  onChange={handleChange}
+                  className="text-black text-[22px] focus:outline-none p-2 w-[100%]"
+                />
+                <button
+                  onClick={handleVisibility}
+                  type="button"
+                  className="cursor-pointer md:text-2xl pr-2"
+                >
+                  {isText ? <FaEye /> : <FaEyeSlash />}
+                </button>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="text-[26px] bg-black text-white py-2 mt-2"
+            >
+              Login
+            </button>
+            <p>{msg}</p>
+          </div>
         </form>
       </div>
     </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
   const [msg, setMsg] = useState("");
+  const [isText, setIsText] = useState(false);
   const { login } = useAuth();
   const navigate = useNavigate();
+
+  const handleVisibility = () => {
+    setIsText((prev) => !prev);
+  };
 
   const handleChange = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value });
@@ -28,10 +34,10 @@ const Register = () => {
     }
   };
   return (
-    <div className="flex h-[50vh] items-center justify-center">
-      <div className="w-[50%] border-2 px-[40px] py-[50px] mt-[200px] border-gray-300">
+    <div className="flex h-[50vh] items-center justify-center p-2">
+      <div className="w-[100%] md:w-[50%] border-2 px-[10px] md:px-[40px] py-[15px] md:py-[50px] mt-[100px] md:mt-[200px] border-gray-300">
         <form onSubmit={handleSubmit}>
-          <h1 className="text-6xl text-center mb-8">Register</h1>
+          <h1 className="text-3xl md:text-6xl text-center mb-8">Register</h1>
           <div className="flex flex-col gap-2">
             <div className="flex flex-col">
               <label className="text-[24px]">Username</label>
@@ -57,13 +63,23 @@ const Register = () => {
 
             <div className="flex flex-col">
               <label className="text-[24px]">Password</label>
-              <input
-                name="password"
-                type="password"
-                onChange={handleChange}
-                className="text-black text-[22px] focus:outline-none border-2 border-gray-300 p-2"
-              />
+              <div className="border-2 border-gray-300 flex">
+                <input
+                  name="password"
+                  type={isText ? "text" : "password"}
+                  onChange={handleChange}
+                  className="text-black text-[22px] focus:outline-none p-2 w-[100%]"
+                />
+                <button
+                  onClick={handleVisibility}
+                  type="button"
+                  className="cursor-pointer md:text-2xl pr-2"
+                >
+                  {isText ? <FaEye /> : <FaEyeSlash />}
+                </button>
+              </div>
             </div>
+
             <button
               type="submit"
               className="text-[26px] bg-black text-white py-2 mt-2"
